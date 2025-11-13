b3f01aac3903530f5b81f72777376e19b5bbe9e0
Marco Sarrió Ferrández <marcovillena24@gmail.com>
2025-11-13 21:25:02 +0100

    feat: add path aliasing in Vite configuration for improved module resolution

--- 02-react/vite.config.js
+++ 02-react/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import path, { __dirname } from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 })
