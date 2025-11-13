43e9dc43a21ca8b99febf1848951730cbf0b628d
Marco Sarrió Ferrández <marcovillena24@gmail.com>
2025-11-13 23:58:24 +0100

    refactor: improve JobListings component by adding a message for no job results

--- 02-react/src/components/JobListings.jsx
+++ 02-react/src/components/JobListings.jsx
@@ -6,6 +6,17 @@ export function JobListings({ jobs }) {
 			<h2>Resultados de búsqueda</h2>
 
 			<div className="jobs-listings">
+				{jobs.length === 0 && (
+					<p
+						style={{
+							textAlign: 'center',
+							padding: '1rem',
+							textWrap: 'balance',
+						}}
+					>
+						No se han encontrado ofertas de empleo.
+					</p>
+				)}
 				{jobs.map((job) => (
 					<JobCard key={job.id} job={job} />
 				))}
